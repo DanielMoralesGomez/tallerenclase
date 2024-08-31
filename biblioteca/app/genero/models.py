2d9c879f35a48b40dd7c415772204733b2f67ef2
DanielMoralesGomez <josedmorales39@gmail.com>
2024-08-31 13:15:55 -0500

    requirements

--- biblioteca/app/genero/models.py
+++ biblioteca/app/genero/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Genero(models.Model):
     nombre = models.CharField(max_length=50, unique=True)
-    descripcion = models.TextField(max_length=300, blank=True, null=True)
+    descripcion = models.CharField(max_length=300, blank=True, null=True)
 
     def __str__(self):
         return self.nombre
