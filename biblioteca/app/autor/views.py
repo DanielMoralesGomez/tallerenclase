fefd91e55a734a69c040fd1598aac44058336db5
JDLB0 <juandiegolopezmedellin123@gmail.com>
2024-08-31 12:57:15 -0500

    Juan DiegoL: Creacion complementos modelo "Autor"

--- biblioteca/app/autor/views.py
+++ biblioteca/app/autor/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Autor
+from .serializer import AutorSerializer
 
-# Create your views here.
+class AutorViewSet(viewsets.ModelViewSet):
+    queryset = Autor.objects.all()
+    serializer_class = AutorSerializer
