b7e42eb2166cf9208972e3963eec2a27f92bdd1d
Juangonzalez05 <jmgr500@gmail.com>
2024-08-31 11:44:14 -0500

    Juan Manuel Gonzalez: creacion model libro

--- biblioteca/app/libro/models.py
+++ biblioteca/app/libro/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from app.genero.models import Genero
+from app.autor.models import Autor
 
-# Create your models here.
+class Libro(models.Model):
+    titulo = models.CharField(max_length=300)
+    autor = models.ForeignKey(Autor, on_delete=models.CASCADE)
+    fecha_publicacion = models.DateField()
+    genero = models.ForeignKey(Genero, on_delete=models.CASCADE)
+    disponible = models.BooleanField(default=True)  
+
+    def __str__(self):
+        return self.titulo
