a4d035ef80cd8dd337d85e5809dcf5d228e8b596
JDLB0 <juandiegolopezmedellin123@gmail.com>
2024-08-31 13:13:53 -0500

    JDLB: correciones

--- biblioteca/app/autor/views.py
+++ biblioteca/app/autor/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets,filters
 from .models import Autor
 from .serializer import AutorSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 
 class AutorViewSet(viewsets.ModelViewSet):
     queryset = Autor.objects.all()
     serializer_class = AutorSerializer
+
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.SearchFilter,
+    ]
+    filterset_fields = ('__all__')
+    search_fields = ('__all__')
