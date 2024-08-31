f75925230f48f7f6ff6e7b5d72b91d3550201252
DanielMoralesGomez <josedmorales39@gmail.com>
2024-08-31 11:38:08 -0500

    Modelo de genero

--- biblioteca/app/genero/models.py
+++ biblioteca/app/genero/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Genero(models.Model):
+    nombre = models.CharField(max_length=50, unique=True)
+    descripcion = models.TextField(max_length=300, blank=True, null=True)
+
+    def __str__(self):
+        return self.nombre
+
