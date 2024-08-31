8ba0e5c922df5fb5c7a8175b2aed1dd18f3becb4
DanielMoralesGomez <josedmorales39@gmail.com>
2024-08-31 12:59:51 -0500

    views y serializers de genero

--- biblioteca/app/genero/views.py
+++ biblioteca/app/genero/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Genero
+from rest_framework import viewsets, filters
+from .serializers import GeneroSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    serializer_class = GeneroSerializer
+    queryset = Genero.objects.all()
+
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.SearchFilter,
+    ]
+    filterset_fields = ('__all__')
+    search_fields = ('__all__')
